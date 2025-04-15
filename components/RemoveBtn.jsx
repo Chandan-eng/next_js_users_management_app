db39f2050e0a21b9096e6f9bcbd39e3dc070b4e8
Chandan Mishra <chandan.mishra@nagarro.com>
2025-04-15 18:27:05 +0530

    remove/delete user feature

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,8 +1,22 @@
+"use client"
 import { HiOutlineTrash } from "react-icons/hi";
+import { useRouter } from "next/navigation";
 
-export default function RemoveBtn() {
+export default function RemoveBtn({id}) {
+    const router = useRouter();
+    const handleDeleteUser = async () => {
+        if (confirm("Are you sure you want to delete this user?")) {
+            const res = await fetch(`http://localhost:3000/api/users/?id=${id}`, {
+                method: "DELETE",
+            });
+           if(res.ok){
+            alert("User deleted successfully");
+            router.refresh();
+           }
+        }
+    };
     return (
-        <button className="text-red-500">
+        <button className="text-red-500" onClick={handleDeleteUser}>
             <HiOutlineTrash size={24} color="error"/>
         </button>
     );
